b4a3be7d80d67bcf6400487367e2aaabad3814d9
Yasin <yasinabdussaburr@gmail.com>
2021-06-16 19:11:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNum) {
+    if (streetNum >= 42) {
+        return streetNum - 42;
+    }
+    else if (streetNum < 42) {
+        return 42 - streetNum;
+    };
+    };
+    function distanceFromHqInFeet(streetNum) {
+        if (streetNum >= 42) {
+            return (streetNum - 42) * 264;
+        }
+        else if (streetNum < 42) {
+            return (42 - streetNum) * 264;
+        }
+    };
+    function distanceTravelledInFeet(num1, num2) {
+        if (num1 > num2) {
+        return (num1 - num2) * 264
+        }
+        else if (num1 <= num2) {
+            return (num2 - num1) * 264
+        }
+    }
+    function calculatesFarePrice(start, destination) {
+        if (Math.abs(start - destination) <= 1) {
+            return 0
+        }
+        else if (Math.abs(start - destination) <= 7) {
+            return (((start - destination) * 264) - 400) * .02
+        }
+        else if (Math.abs(start - destination) <= 8 ) {
+            return 25
+        }
+        else if (start - destination >= 10) {
+        return 'cannot travel that far'
+        }
+    }
